ccda8ad7095ab7660e9b2456e41c72e2524d013b
alicialundberg <sara.alicia.lundberg@gmail.com>
2020-06-04 22:07:39 +0200

    Update header.php

--- maskindistans/header.php
+++ maskindistans/header.php
@@ -9,12 +9,15 @@
         <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
         <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
         <?php wp_head(); ?>
-        <!-- <script src="/js/store.js" async></script> -->
        
     </head>
     <body>
         <header>
+            <div class="top-nav">
+                <div id="weglot_here"></div>
+            </div>
           <div class="header-image">
+         
           <?php /* Hämtar loggan som läggs till i admin desktop */
             $custom_logo_id = get_theme_mod( 'custom_logo' );
             $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
@@ -35,7 +38,8 @@
                 <div class="shopping-cart">
                     <a href="cart"  id="shopcart" ><i class="fa fa-shopping-cart"></i></a> 
                 </div>
-            <!-- </div> -->
+          
+  
             
             
         </div>
